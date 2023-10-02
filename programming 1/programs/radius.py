d48320d7fc6ba2516afc4fe08f0b11128118e4d4
Owoscaw <oscarmartinpearce@gmail.com>
2023-10-02 14:53:17 +0100

    lecture 1

--- programming 1/programs/radius.py
+++ programming 1/programs/radius.py
@@ -1,24 +1,26 @@
-def calcDistance(x1, y1, x2, y2):
-    return ((x1-x2)**2 + (y1-y2)**2)**0.5
-
-print(calcDistance(2, 3, 1.4, 4) <= 1.5)
-
-
 class Point():
 
     def __init__(self, x, y):
-        self.__x = x
-        self.__y = y
+        self.x = x
+        self.y = y
 
     def __add__(self, point):
         return Point(self.x + point.x, self.y + point.y)
     
     def __sub__(self, point):
         return Point(self.x - point.x, self.y - point.y)
-
     
     def displayPoint(self):
         print("({}, {})".format(self.x, self.y))
 
     def getDistTo(self, point):
-        return
+        distVector = self - point
+        return (distVector.x**2 + distVector.y**2)**0.5
+    
+def closeEnough(point1, point2, radius):
+    return point1.getDistTo(point2) <= radius
+
+point1 = Point(2,3)
+point2 = Point(1.4,4)
+
+print(closeEnough(point1, point2, 1.5))
