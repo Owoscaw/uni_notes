48288adc2593e4befd4621792fa5875e365f30af
Owoscaw <oscarmartinpearce@gmail.com>
2023-10-02 14:50:58 +0100

    vault backup: 2023-10-02 14:50:58

--- programming 1/programs/testing program.py
+++ programming 1/programs/testing program.py
@@ -1,1 +1,24 @@
-print("hello world!!!")
+def calcDistance(x1, y1, x2, y2):
+    return ((x1-x2)**2 + (y1-y2)**2)**0.5
+
+print(calcDistance(2, 3, 1.4, 4) <= 1.5)
+
+
+class Point():
+
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def __add__(self, point):
+        return Point(self.x + point.x, self.y + point.y)
+    
+    def __sub__(self, point):
+        return Point(self.x - point.x, self.y - point.y)
+
+    
+    def displayPoint(self):
+        print("({}, {})".format(self.x, self.y))
+
+    def getDistTo(self, point):
+        return
