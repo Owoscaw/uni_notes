bc4e391bc34d3ab71e174d2034509e0d904389c9
Owoscaw <oscarmartinpearce@gmail.com>
2023-11-02 17:17:56 +0000

    vault backup: 2023-11-02 17:17:56

--- programming 1/programs/G-Jelim.py
+++ programming 1/programs/G-Jelim.py
@@ -12,36 +12,11 @@ def is_zero_row(R):
             return False
     return True
 
-def is_in_RREF(A):
-    has_1_in_col = [-1]*len(A[0])
-    
-    for i in range(len(A)-1):
-        leading = True
+def permute(A,r,s):
+    return [A[s] if i == r else A[r] if i == s else A[i] for i in range(len(A))]
 
-        if is_zero_row(A[i]) and not is_zero_row(A[(i+1)%len(A)]):
-            return False
-
-        for j in range(len(A[i])):
-            print(has_1_in_col)
-
-            if A[i][j] != 0 and has_1_in_col[j] != -1:
-                return False
-            
-
-            if A[i][j] == 1 and leading:
-                if j <= max(has_1_in_col):
-                    return False
-                has_1_in_col[j] = j
-                leading = False
-            elif A[i][j] == 0 and leading:
-                leading = True
-            elif A[i][j] != 0 and leading:
-                return False
-            elif A[i][j] != 0 and not leading and has_1_in_col[j] != -1:
-                return False
-    return True
+print(permute([[0,1,1],[1,0,1],[1,1,1]], 0, 1))
         
         
                 
 
-print(is_in_RREF([[3,1],[0,1]]))
