9d0ebaaf55b3e67e47344091cd37e05589f156aa
Owoscaw <oscarmartinpearce@gmail.com>
2023-11-30 16:17:17 +0000

    vault backup: 2023-11-30 16:17:17

--- programming 1/programs/testing.py
+++ programming 1/programs/testing.py
@@ -1,21 +1,21 @@
-from matplotlib import pyplot as plt
-import math as m
+from math import sin,cos,sqrt
 
-def showplots(funcs, start, end, detail):
+def is_close_to(a,b,eps):
+    return abs(a-b)<eps
 
-    xVals = [start + i*((end-start)/detail) for i in range(detail)]
-    yValList = [[func(xVal) for xVal in xVals] for func in funcs]
-    lineType = ["-r","-b","+r","+b"]
-
-    for i in range(len(funcs)):
-        plt.plot(xVals, yValList[i], lineType[i])
+def equationroot():
+    m=0
+    a=3
+    b=4
+    while not is_close_to(a,b,10e-11):
+        m=(a+b)/2
+        if sin(m) + sin((6)**(0.5)*m) > 0:
+            a=m
+        else:
+            b=m
+    return m
     
-    plt.axhline(y=0, color="blue", ls="--")
-    plt.xlabel("x")
-    plt.ylabel("cos(x^3)")
-    plt.show()
+def strequationroot():
+    return "{:.8f}".format(equationroot())
 
-def cosine_x_cubed(x):
-    return m.cos(x**3)
-
-showplots([cosine_x_cubed], -0.5, 4, 10000)
+print(strequationroot())
