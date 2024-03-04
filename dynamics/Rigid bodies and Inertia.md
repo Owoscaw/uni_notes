faf108d27afc1f98cde2128fa847ea34f137e518
Owoscaw <oscarmartinpearce@gmail.com>
2024-03-04 14:20:16 +0000

    vault backup: 2024-03-04 14:20:16

---
+++ dynamics/Rigid bodies and Inertia.md
@@ -0,0 +1,4 @@
+
+A rigid body is a collection of masses such that the distance between the masses is fixed. If the body is not rotating, it behaves like a single particle. Consider masses $m_1,\dots,m_N$ at positions $\underline r_1,\dots, \underline r_N$. The single particle has mass $M=m_1+\dots+m_N$ and the position of the centre of mass is $\underline R=\frac{1}{M}(m_1 \underline r_1+\dots+m_N \underline r_N)$, the weighted position of masses.
+
+Consider a rigid body rotating about a fixed axis, each $m_i$ moves in a circle as an angle 
--- programming 1/programs/step_functions.py
+++ programming 1/programs/step_functions.py
@@ -1,6 +1,5 @@
 from matplotlib import pyplot as plt
 import numpy as np
-import time as t
 
 def nth_partition_of_b(n, b, c):
     pVals = [b**(i/n) for i in range(0,n+1)]
@@ -9,15 +8,17 @@ def nth_partition_of_b(n, b, c):
         plt.plot([pVals[i], pVals[i+1]], [b**((i*c)/(n-1))]*2)
 
 def increase_partition(limit, b, c):
+
+    plt.ion()
     xVals = np.linspace(1,b,1000)
     plt.plot(xVals, [x**c for x in xVals])
 
-    plt.ion()
     for i in range(1,limit):
         nth_partition_of_b(i+1, b, c)
-        t.sleep(1000)
+        plt.pause(1)
+        
+    
 
-    plt.show()
 
 
 increase_partition(10, 5, 2)
