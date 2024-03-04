8d0f730af7f0417bca7f2818a65fb8275a8fe64d
Owoscaw <oscarmartinpearce@gmail.com>
2024-03-04 14:30:16 +0000

    vault backup: 2024-03-04 14:30:16

--- programming 1/programs/step_functions.py
+++ programming 1/programs/step_functions.py
@@ -5,20 +5,20 @@ def nth_partition_of_b(n, b, c):
     pVals = [b**(i/n) for i in range(0,n+1)]
 
     for i in range(0,len(pVals)-1):
-        plt.plot([pVals[i], pVals[i+1]], [b**((i*c)/(n-1))]*2)
+        plt.plot([pVals[i], pVals[i+1]], [b**((i*c)/(n-1))]*2, "blue")
+
+    xVals = np.linspace(1,b,1000)
+    plt.plot(xVals, [x**c for x in xVals], "red")
+    plt.pause(0.05)
+    plt.clf()
 
 def increase_partition(limit, b, c):
-
     plt.ion()
-    xVals = np.linspace(1,b,1000)
-    plt.plot(xVals, [x**c for x in xVals])
-
     for i in range(1,limit):
         nth_partition_of_b(i+1, b, c)
-        plt.pause(1)
         
     
 
 
 
-increase_partition(10, 5, 2)
+increase_partition(1000, 5, 2)
