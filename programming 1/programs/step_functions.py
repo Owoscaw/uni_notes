248222ad8253b92710386e07fd47a0bfca0a5f1d
Owoscaw <oscarmartinpearce@gmail.com>
2024-03-04 14:10:16 +0000

    vault backup: 2024-03-04 14:10:16

--- programming 1/programs/step_functions.py
+++ programming 1/programs/step_functions.py
@@ -1,11 +1,23 @@
 from matplotlib import pyplot as plt
+import numpy as np
+import time as t
 
-def show_nth_partition_of_b(n, b, c):
-    xVals = [b**(i/n) for i in range(0,n+1)]
+def nth_partition_of_b(n, b, c):
+    pVals = [b**(i/n) for i in range(0,n+1)]
 
-    for i in range(0,len(xVals)-1):
-        plt.plot([xVals[i], xVals[i+1]], [b**((i*c)/(n-1))]*2)
+    for i in range(0,len(pVals)-1):
+        plt.plot([pVals[i], pVals[i+1]], [b**((i*c)/(n-1))]*2)
+
+def increase_partition(limit, b, c):
+    xVals = np.linspace(1,b,1000)
+    plt.plot(xVals, [x**c for x in xVals])
+
+    plt.ion()
+    for i in range(1,limit):
+        nth_partition_of_b(i+1, b, c)
+        t.sleep(1000)
 
     plt.show()
 
-show_nth_partition_of_b(3,5,2)
+
+increase_partition(10, 5, 2)
