8d709eac12dcca1c329ed75446bc097e9f292a5b
Owoscaw <oscarmartinpearce@gmail.com>
2024-10-10 16:30:04 +0100

    vault backup: 2024-10-10 16:30:04

--- year 2/mathematical modelling 2/MMII/week1/swap_odd_even.py
+++ year 2/mathematical modelling 2/MMII/week1/swap_odd_even.py
@@ -1,11 +1,11 @@
 
 def swap_odd_even(array):
     if len(array)%2 != 0:
-        return
-    result = []
-    evens = array[0:-1:2]
-    odds = array[1:0:2]
-    result.append([evens, odds])
+        return []
+    result = [0]*len(array)
+    evens = array[0::2]
+    odds = array[1::2]
+    result = array[:-3:-1]
     return result
 
 print(swap_odd_even([1,4,7,2,3,6]))
