485ba8f741f1f048c8decfd680af005a1d875802
Owoscaw <oscarmartinpearce@gmail.com>
2024-10-12 14:33:25 +0100

    vault backup: 2024-10-12 14:33:25

--- year 2/mathematical modelling 2/MMII/week1/distance.py
+++ year 2/mathematical modelling 2/MMII/week1/distance.py
@@ -4,6 +4,6 @@ def distance(distances):
     shiftedDistances = distances[:]
     shiftedDistances[1:] = shiftedDistances[:-1]
     shiftedDistances[0] = distances[-1]
-    return np.array(distances) - np.array(shiftedDistances)
+    return abs(np.array(distances) - np.array(shiftedDistances))
 
 print(distance([10,14,30,45,53]))
