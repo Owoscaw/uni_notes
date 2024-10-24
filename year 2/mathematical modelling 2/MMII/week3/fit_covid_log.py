7ad4fa7523b94aa88ed88e4e5e6e8a2c2aa718ae
Owoscaw <oscarmartinpearce@gmail.com>
2024-10-24 16:52:30 +0100

    vault backup: 2024-10-24 16:52:30

--- year 2/mathematical modelling 2/MMII/week3/fit_covid_log.py
+++ year 2/mathematical modelling 2/MMII/week3/fit_covid_log.py
@@ -7,13 +7,13 @@ data = [i[1] for i in csvData]
 days = np.arange(len(data))
 
 def func(d, A, k):
-    return A*np.exp(k*d)
+    return np.log(A) + k*d
 
-popt, pcov = scipy.optimize.curve_fit(func, days, data, p0=[1000, 0.01])
+popt, pcov = scipy.optimize.curve_fit(func, days, np.log(data), p0=[1000, 0.01])
 
 print("A = {}, k = {}".format(popt[0],popt[1]))
 print("tau = {}".format(np.log(2)/popt[1]))
 
 plt.semilogy(days, data, "b*")
-plt.semilogy(days, func(days, *popt), "r--", label="fit to A*exp(k*d)")
+plt.semilogy(days, np.exp(func(days, *popt)), "r--", label="fit to A*exp(k*d)")
 plt.show()
