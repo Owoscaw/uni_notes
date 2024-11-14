2ba71ee0e54253e02c57e440c8e0a21939514fc3
Owoscaw <110049655+Owoscaw@users.noreply.github.com>
2024-11-14 16:37:41 +0000

    vault backup: 2024-11-14 16:37:41

--- year 2/mathematical modelling 2/MMII/week6/simple_relaxation.py
+++ year 2/mathematical modelling 2/MMII/week6/simple_relaxation.py
@@ -1,31 +1,41 @@
 
-class Matrix:    
-    mat = []
+# class Matrix:    
+#     mat = []
 
-    def __init__(self, *args):
+#     def __init__(self, *args):
         
-        self.mat = [i for i in args]
+#         self.mat = [i for i in args]
         
-    def printMatrix(self):
-        for i in self.mat:
-            for j in i:
-                print(str(j) + " ", end="")
-            print("")
+#     def printMatrix(self):
+#         for i in self.mat:
+#             for j in i:
+#                 print(str(j) + " ", end="")
+#             print("")
 
-    def __add__(self, other):
-        aMat = Matrix(*self.mat)
-        for i in range(len(self.mat)):
-            for j in range(len(self.mat[i])):
-                aMat.mat[i][j] = self.mat[i][j] + other.mat[i][j]
+#     def __add__(self, other):
+#         aMat = Matrix(*self.mat)
+#         for i in range(len(self.mat)):
+#             for j in range(len(self.mat[i])):
+#                 aMat.mat[i][j] = self.mat[i][j] + other.mat[i][j]
 
-        return aMat
+#         return aMat
+    
+#     def __mult__(self, other):
+
+def matMult(mat, v):
+    result = [0]*len(v)
+    for i in range(len(v)):
+        for j in range(len(v)):
+            result[i] += mat[i][j]*v[j]
+
+    return result
+
+print(matMult([[0,0],[0,0]], [3,3]))
 
     
+def iterate(N, vJ, A, B, iMax):
 
+
+    for i in range(iMax):
+        N += 1
             
-
-zMat = Matrix([1,0],[1,0])
-iMat = Matrix([1,0],[0,1])
-
-cMat = zMat + iMat
-cMat.printMatrix()
