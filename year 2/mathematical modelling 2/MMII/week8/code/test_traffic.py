3b9846cc41c70fd9d34b9a17b8960e07c942c050
Owoscaw <110049655+Owoscaw@users.noreply.github.com>
2024-12-03 14:55:34 +0000

    vault backup: 2024-12-03 14:55:34

--- year 2/mathematical modelling 2/MMII/week8/code/test_traffic.py
+++ year 2/mathematical modelling 2/MMII/week8/code/test_traffic.py
@@ -5,5 +5,5 @@ road_len = 200
 p_slowdown = 0.1
 N = 200
 Nrelax = 100
-tl.simulate(density, vmax, road_len, p_slowdown, N, Nrelax, '')
+#tl.simulate(density, vmax, road_len, p_slowdown, N, Nrelax, '')
 tl.flow_density(0.01, 1, 100, vmax, road_len, p_slowdown, '')
