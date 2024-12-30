6c72be39d24570144b2238da57731daa2d99dd04
Owoscaw <110049655+Owoscaw@users.noreply.github.com>
2024-12-30 13:29:10 +0000

    vault backup: 2024-12-30 13:29:10

--- year 2/mathematical modelling 2/MMII/week8/code/run_traffic_array.py
+++ year 2/mathematical modelling 2/MMII/week8/code/run_traffic_array.py
@@ -1,9 +1,9 @@
-import traffic_array as ta
+import traffic as ta
 density = 0.4
 vmax=4
 road_len = 200
 p_slowdown = 0.1
 N = 200
 Nrelax = 100
-ta.simulate(density, vmax, road_len, p_slowdown, N, Nrelax)
+#ta.simulate(density, vmax, road_len, p_slowdown, N, Nrelax, "")
 ta.flow_density(0.01, 1, 100, vmax, road_len, p_slowdown, '')
