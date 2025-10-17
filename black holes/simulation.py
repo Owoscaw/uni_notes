1cdfa67f43a113803c96a869bbe9c7e749cdef62
Owoscaw <oscarmartinpearce@gmail.com>
2025-10-17 06:24:22 +0100

    vault backup: 2025-10-17 06:24:22

--- black holes/simulation.py
+++ black holes/simulation.py
@@ -1,4 +1,18 @@
 import tkinter as tk
 
-class Penrose():
-    
+class R3simulation():
+    __window = None
+
+    def __init__(self):
+        self.__window = tk.Tk()
+        pass
+
+    def run(self):
+
+        self.__window.state("zoomed")
+        self.__window.mainloop()
+
+
+
+test = R3simulation()
+test.run()
