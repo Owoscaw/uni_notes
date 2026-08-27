b7b7f14da4d7d5ea1d5b863a43f228a68bbfea5b
Owoscaw <110049655+Owoscaw@users.noreply.github.com>
2026-08-27 23:40:51 +0100

    vault backup: 2026-08-27 23:40:51

--- simulations/gravity visualiser/graviton.py
+++ simulations/gravity visualiser/graviton.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
 M_ratio = 0.01
+LOD = 500
+scale = 1.5
 
 # Astrophysical Constants
 G = 6.67430e-11        # Gravitational constant (m^3 kg^-1 s^-2)
@@ -14,6 +17,27 @@ pi_E = M_E/(M_E + M_S)
 pi_S = M_S/(M_E + M_S)
 x_S = -pi_E * R
 x_E = pi_S * R
+
+# Creating low LOD meshgrid
+low_x = R * np.linspace(-scale * (1.0 - x_S), scale * (1.0 - x_E), LOD)
+low_y = R * np.linspace(-1.0, 1.0, LOD)
+low_X, low_Y = np.meshgrid(low_x, low_y)
+
+# Finding equilateral points
+L_4 = R * scale * np.array([0.5 - pi_E, np.sqrt(3)/2])
+L_5 = R * scale * np.array([0.5 - pi_E, -np.sqrt(3)/2])
+
+# Finding colinear points
+def colinear_cubic(x):
+    return x - (1 - pi_E)/np.abs(x + pi_E)**3 * (x + pi_E) - pi_E/np.abs(x - 1 + pi_E)**3 * (x - 1 + pi_E)
+
+zeroes = R * scale * (scipy.optimize.newton(colinear_cubic, [-1.0, 0, 1.0]) - x_S)
+print(zeroes)
+
+
+# Mark Lagrange points
+zero_potentials = np.concatenate((np.array([[i, 0] for i in zeroes]), np.array([L_4, L_5])))
+print(zero_potentials)
 
 # Mark the celestial bodies
 plt.plot(x_S, 0, 'o', color="orange", markersize=20, label="Sun")
